5008c470a362f4a298399902cff751ad214c289c
Guru Saran M <gurusaranm0025@gmail.com>
2025-10-04 11:56:40 +0530

    returns the command specific, when faced withb one now

--- pkg/utils/commandExec.go
+++ pkg/utils/commandExec.go
@@ -9,7 +9,7 @@ func ExecCommand(command string) (string, error) {
 	cmd := exec.Command("bash", "-c", command)
 	output, err := cmd.CombinedOutput()
 	if err != nil {
-		return "", err
+		return string(output), err
 	}
 
 	return string(output), nil
