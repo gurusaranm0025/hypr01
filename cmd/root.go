3dec1aec4c9b4e6c14d7538b0ff4873089a35758
Guru Saran M <gurusaranm0025@gmail.com>
2025-10-04 12:31:46 +0530

    0.5.4-5 (alpha)

--- cmd/root.go
+++ cmd/root.go
@@ -13,7 +13,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var VERSION = "0.5.3-5 (alpha)"
+var VERSION = "0.5.4-5 (alpha)"
 
 var brightness, sound, mute, hypridle string
 var initialise, wallpaperGUI, ver bool
