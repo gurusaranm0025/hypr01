2ad13221c196937e08e04c158b3ceb3422b3f69f
Guru Saran M <gurusaranm0025@gmail.com>
2026-01-15 20:48:52 +0530

    reorganised code

--- pkg/modules/Logout/logout.go
+++ pkg/modules/Logout/logout.go
@@ -8,21 +8,59 @@ import (
 	"strings"
 )
 
-func Logout(layout int) error {
-	var width, height, hyprBorder, cols, scale int
+type LogoutValues map[string]string
+
+func GetLogoutValues(layout int) (LogoutValues, error) {
+	var width, height, hyprBorder, scale int
 	var err error
 
-	home := utils.GetHomeDir()
-
 	if width, height, scale, err = display.GetScreenresolution(); err != nil {
-		return err
+		return nil, err
 	}
 
 	scale = scale * 100
 	if hyprBorder, err = display.GetHyprBorder(); err != nil {
-		return err
+		return nil, err
 	}
 
+	fontSize := fmt.Sprintf("%d", height*2/100)
+	buttonRadius := fmt.Sprintf("%d", hyprBorder*8)
+	activeButtonRadius := fmt.Sprintf("%d", hyprBorder*5)
+
+	logoutValues := LogoutValues{
+		"${fontSize}":             fontSize,
+		"${button_radius}":        buttonRadius,
+		"${active_button_radius}": activeButtonRadius,
+		"${HOME}":                 utils.GetHomeDir(),
+	}
+
+	switch layout {
+	case 1:
+		margin := fmt.Sprintf("%d", height*28/scale)
+		hover := fmt.Sprintf("%d", height*23/scale)
+		logoutValues["${margin}"] = margin
+		logoutValues["${hover}"] = hover
+	case 2:
+		x_margin := fmt.Sprintf("%d", width*35/scale)
+		y_margin := fmt.Sprintf("%d", height*25/scale)
+		x_hover := fmt.Sprintf("%d", width*32/scale)
+		y_hover := fmt.Sprintf("%d", height*20/scale)
+
+		logoutValues["${x_margin}"] = x_margin
+		logoutValues["${y_margin}"] = y_margin
+		logoutValues["${x_hover}"] = x_hover
+		logoutValues["${y_hover}"] = y_hover
+	}
+
+	return logoutValues, nil
+}
+
+func Logout(layout int) error {
+	var logoutValues LogoutValues
+	var cols int
+	var err error
+
+	home := utils.GetHomeDir()
 	layoutPath := fmt.Sprintf("%s/.config/wlogout/layout_%d", home, layout)
 	stylesPath := fmt.Sprintf("%s/.config/wlogout/style_%d.css", home, layout)
 
@@ -31,33 +69,19 @@ func Logout(layout int) error {
 		return err
 	}
 
-	fontSize := fmt.Sprintf("%d", height*2/100)
-	buttonRadius := fmt.Sprintf("%d", hyprBorder*8)
-	activeButtonRadius := fmt.Sprintf("%d", hyprBorder*5)
+	if logoutValues, err = GetLogoutValues(layout); err != nil {
+		return err
+	}
 
-	stylesContent = strings.ReplaceAll(stylesContent, "${fontSize}", fontSize)
-	stylesContent = strings.ReplaceAll(stylesContent, "${active_button_radius}", activeButtonRadius)
-	stylesContent = strings.ReplaceAll(stylesContent, "${button_radius}", buttonRadius)
-	stylesContent = strings.ReplaceAll(stylesContent, "${HOME}", home)
+	for old, new := range logoutValues {
+		stylesContent = strings.ReplaceAll(stylesContent, old, new)
+	}
 
 	switch layout {
 	case 1:
 		cols = 6
-		margin := fmt.Sprintf("%d", height*28/scale)
-		hover := fmt.Sprintf("%d", height*23/scale)
-		stylesContent = strings.ReplaceAll(stylesContent, "${margin}", margin)
-		stylesContent = strings.ReplaceAll(stylesContent, "${hover}", hover)
 	case 2:
 		cols = 2
-		x_margin := fmt.Sprintf("%d", width*35/scale)
-		y_margin := fmt.Sprintf("%d", height*25/scale)
-		x_hover := fmt.Sprintf("%d", width*32/scale)
-		y_hover := fmt.Sprintf("%d", height*20/scale)
-
-		stylesContent = strings.ReplaceAll(stylesContent, "${x_margin}", x_margin)
-		stylesContent = strings.ReplaceAll(stylesContent, "${y_margin}", y_margin)
-		stylesContent = strings.ReplaceAll(stylesContent, "${x_hover}", x_hover)
-		stylesContent = strings.ReplaceAll(stylesContent, "${y_hover}", y_hover)
 	}
 
 	cssPath := filepath.Join(home, ".cache/HyprOne/style.css")
