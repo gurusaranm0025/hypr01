d537fbb4c807cc8c8724d11f080416efa38d3fd9
Guru Saran M <gurusaranm0025@gmail.com>
2026-01-15 23:42:12 +0530

    config path added

--- pkg/common/paths.go
+++ pkg/common/paths.go
@@ -11,6 +11,8 @@ var ALL_WALLS_DIR_PATH = filepath.Join(CONFIG_DIR_PATH, "walls")
 var CURRENT_WALL_DIR_PATH = filepath.Join(CONFIG_DIR_PATH, "current_wall")
 var GIT_CLONE_DIR_PATH = filepath.Join(utils.GetHomeDir(), ".config/hyprone/.temp/git_clone")
 
+var HYPR01_CONFIG_PATH = filepath.Join(CONFIG_DIR_PATH, "config.json")
+
 var PathCounterParts = map[string]string{
 	"${CURRENT_WALL_DIR_PATH}": CURRENT_WALL_DIR_PATH,
 	"${CONFIG_DIR_PATH}":       CONFIG_DIR_PATH,
